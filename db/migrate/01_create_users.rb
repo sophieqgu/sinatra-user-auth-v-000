b15a67c8f61387327e85e1a59c8edbe5b3f6e966
Sophie Gu <sophieqgu@gmail.com>
2019-09-16 01:49:43 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -2,4 +2,8 @@ class CreateUsers < Sinatra::Migration
   def change 
     create_table :users do |t|
       t.string :name 
-      t.string
+      t.string :email 
+      t.string :password 
+    end 
+  end 
+end 
